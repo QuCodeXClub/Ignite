e355d65fe76ecbed3b072e3578dc5ae1183df758
Shobhit Singh <112196213+shobhit115@users.noreply.github.com>
2026-09-12 14:36:35 +0530

    Revert "Event Timeline Addition "

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import Navbar from './components/Navbar';
-import HeroModel from './components/HeroModel';
-import AboutSection from './components/AboutSection';
-import EventsSection from './components/EventsSection';
-import ScheduleSection from './components/ScheduleSection';
 import Hero from './components/Hero';
 import FooterElements from './components/FooterElements';
 
 function App() {
   return (
     <div className="app-container">
-      <div className="canvas-container">
-        <HeroModel />
-      </div>
       <Navbar />
       <Hero />
-      <AboutSection />
-      <EventsSection />
-      <ScheduleSection />
       <FooterElements />
     </div>
   );
